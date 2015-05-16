391b466426cb5ec6f07a7bb1ac1a282d3fd4309a
pmiangolarra <pmiangolarra@PMIANGOLARRAPW7.indra.es>
2015-05-16 17:37:32 +0200

    Warehouse class v1.0 finished: - ArrayList structure to store Parts - Constant int to determine the amount of Parts stored in Inventory. - method to paint all Parts stored, request Part´s paint method - method fillInventory to fill Inventory with random Parts, it will use the static method generatePart from Part class (TBImplemented).

--- src/main/java/es/upm/fi/emse/Warehouse.java
+++ src/main/java/es/upm/fi/emse/Warehouse.java
@@ -3,10 +3,18 @@ package es.upm.fi.emse;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Warehouse extends Component {
 
+	private static int INVENTORY_SIZE = 3;
 	private static final long serialVersionUID = -4985756162707607745L;
+	private ArrayList<Part>  myInventory;
+	
+	public Warehouse(){		
+		this.myInventory = fillInventory();
+	}
 
 	public void paint(Graphics g) {
 		g.setColor(Color.BLUE);
@@ -14,6 +22,27 @@ public class Warehouse extends Component {
 		int padding = 10;
 		g.drawRect(padding, padding, getWidth() - (padding * 2), getHeight() - (padding * 2));
 		g.drawString("WAREHOUSE", padding * 2, padding * 2);
+		
+		paintPartsInInventory(g);
+	}
+	
+	private void paintPartsInInventory(Graphics g){
+		// Paint all Parts stored in my inventory aligned horizontally
+		int x = 0;
+		int y = 0;
+		for (Part p : myInventory){
+		//	p.paint(x,y);
+			x = x + 10;
+		}	
 	}
 
+	private ArrayList<Part> fillInventory(){
+		
+		ArrayList<Part> myInv;
+		myInv = new ArrayList<Part>();
+		for (int i = 0; i<INVENTORY_SIZE;i++){
+		//	myInv.add(Part.generateRandomPart());
+		}
+		return myInv;
+	}
 }
