2c5170a7fad51e1d44235986fdc247d8bc887231
Nika <nikcolli@gmail.com>
2015-05-17 15:27:35 +0200

    Added getter and setter in Task class

--- src/main/java/es/upm/fi/emse/Task.java
+++ src/main/java/es/upm/fi/emse/Task.java
@@ -1,8 +1,18 @@
 package es.upm.fi.emse;
 
 public class Task {
-	protected Part part;
+	
+	private Part part;
+	
+	public void setPart(Part part) {
+		this.part = part;
+	}
+	
+	public Part getPart() {
+		return part;
+	}
 
 	public void draw() {
+		//TODO implement method
 	}
 }
